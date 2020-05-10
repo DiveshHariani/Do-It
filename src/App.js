a1153b15739b9da873afbe6c4b810ac4dde7cd8b
Divesh <diveshharyani92@gmail.com>
2020-05-10 17:10:13 +0530

    Front end complete!

--- src/App.js
+++ src/App.js
@@ -1,46 +1,81 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
 import Header from './components/Header'
 import Task from './components/Task'
 import AddButton from './components/AddButton'
 
-function renderTask(task) {
-  return (<Task title={task.title} due={task.due} details={task.details} />);
-}
-function App() {
-  // const items = [{title:'Learn React',due:'Today',details:'Learn React today and complete to-do front end'},
-  //                 {title:'Learn React',due:'Today',details:'Learn React today and complete to-do front end'}]
-  let [items,changeItems] = useState([{title:'Learn React',due:'Today',details:'Learn React today and complete to-do front end'},
-                                     {title:'Learn React',due:'Today',details:'Learn React today and complete to-do front end'}])
-  function addNewItem() {
-    let newItem = {
-      title: ' ',
-      due: ' ',
-      details: ' '
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+                  items: [],
+                  status:[]
+                  };
+    }  
+  
+  addNewItem = (event) => {
+    event.preventDefault();
+    var newTask = {
+      id: this.state.items.length,
+      title: '',
+      due:'',
+      details:''
     };
-    newItem.title = prompt('Enter title:');
-    newItem.due = prompt('Enter due:');
-    newItem.details = prompt('Enter details: ');
-    console.log(newItem);
-    changeItems(items.push(newItem));
-    console.log(items);
+    newTask.title = prompt('Enter title:');
+    newTask.due = prompt('Enter due:');
+    newTask.details = prompt('Enter details: ');
+    this.state.items.push(newTask);
+    this.state.status.push(0)
+    const newArr = this.state.items;
+    const statusArr = this.state.status;
+    console.log(newArr);
+    this.setState(
+      {
+        items: newArr,
+        status: statusArr
+      }
+    );
+    console.log(this.state.items);
+  }
+
+  
+  handleComplete = (id) => {
+    console.log(id);
+    const statusArr = this.state.status.slice();
+    statusArr[id] = 1;
+    const itemsArr = this.state.items;
+    this.setState({
+      items: itemsArr,
+      status: statusArr
+    });
+  }
+
+  renderTask = (task) => {
+    if(this.state.status[task.id] === 0) {
+      return (<Task key={task.id} title={task.title} due={task.due} details={task.details} onClick={() => this.handleComplete(task.id)} />);
+    }
   }
 
 
-  return (
+  render() {
+    const {items} = this.state;
+    return (
     <div>
+      {console.log('In return')}
       <div>
         <Header />
       </div>
       <div className='App-content'>
-        {items.map(renderTask)}
-
+        {items.map(this.renderTask)}
       </div>
       <div className='App-add'>
-        <AddButton onClick = {addNewItem}/>
+        <AddButton onClick={this.addNewItem} />
       </div>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,30 +1,18 @@
-import React, {useState} from 'react'
+import React from 'react'
 
 import './Task.css'
 
 function Task(props) {
-
-    const [status , setStatus] = useState(false);
-
-    function completed(e) {
-        e.preventDefault();
-        console.log('Clicked')
-        console.log(status);        ///PROBLEM
-        setStatus(true);
-        console.log(status);
-    }
     return (
-    
-        <div className = 'card'>
-            <div className='card-body customCard'>
-                <h2 className='card-title'>{props.title}</h2>
-                <h4 className='card-subtitle'>{props.due}</h4>
-                <p className='card-text'>
+        <div className = 'custom-card'>
+            <div className='custom-body'>
+                <h3 className='custom-card-title'>{props.title}</h3>
+                <h4 className='custom-card-subtitle'>{props.due}</h4>
+                <p className='custom-card-text'>
                     {props.details}
                 </p>
-                <div className='submission'>
-                   <button type='button' className='btn btn-success' onClick={completed}>Completed</button>
-                </div>
+                <button type='button' className='btn btn-success' onClick={props.onClick}>Completed</button>
+                
             </div>
         </div> 
     )
