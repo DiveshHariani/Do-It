1a70315c75ad0616e24b97b92507b6f2745db84b
Divesh <diveshharyani92@gmail.com>
2020-05-14 17:46:30 +0530

    App ready

--- src/App.js
+++ src/App.js
@@ -11,9 +11,19 @@ class App extends React.Component {
     super(props);
     this.state = {
                   items: [],
-                  status:[]
+                  status:[],
+                  isLoaded: true
                   };
-    }  
+    }
+  
+
+  componentDidMount() {
+    let i = 0;
+    fetch('http://localhost:5000/tasks')
+    .then( res => res.json() )
+    .then( json => this.setState( {items:json.map( (ele) => {ele.id = i++; return ele}),status: Array(json.length).fill(0), isLoaded: true} ))
+    .catch( err => console.log(err));
+  }
   
   addNewItem = (event) => {
     event.preventDefault();
@@ -26,6 +36,11 @@ class App extends React.Component {
     newTask.title = prompt('Enter title:');
     newTask.due = prompt('Enter due:');
     newTask.details = prompt('Enter details: ');
+    fetch('http://localhost:5000/tasks', {
+      method:'POST',
+      headers:{'Content-Type':"application/json"},
+      body : JSON.stringify(newTask)
+    }).then(console.log('POST complete')).catch( err => console.log(err) );
     this.state.items.push(newTask);
     this.state.status.push(0)
     const newArr = this.state.items;
@@ -41,11 +56,14 @@ class App extends React.Component {
   }
 
   
-  handleComplete = (id) => {
+  handleComplete = (id,title) => {
     console.log(id);
     const statusArr = this.state.status.slice();
     statusArr[id] = 1;
     const itemsArr = this.state.items;
+    fetch('http://localhost:5000/tasks/' + title,{
+      method: 'DELETE'
+    }).then('Completed').catch(err => console.log(err));
     this.setState({
       items: itemsArr,
       status: statusArr
@@ -54,16 +72,22 @@ class App extends React.Component {
 
   renderTask = (task) => {
     if(this.state.status[task.id] === 0) {
-      return (<Task key={task.id} title={task.title} due={task.due} details={task.details} onClick={() => this.handleComplete(task.id)} />);
+      return (<Task key={task.id} title={task.title} due={task.due} details={task.details} onClick={() => this.handleComplete(task.id,task.title)} />);
     }
   }
 
 
   render() {
-    const {items} = this.state;
+    const {items, status, isLoaded} = this.state;
+    console.log(items)
+    if(!isLoaded)
+    {
+      return <h2>Loading...</h2>
+    }
     return (
     <div>
       {console.log('In return')}
+      {console.log(items)}
       <div>
         <Header />
       </div>
